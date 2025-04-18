525c52bbed315ae5741752fa1532b0eb3ca304fc
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-04-18 14:28:57 +0330

    add practices

---
+++ prac0.py
@@ -0,0 +1,9 @@
+sentence = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs."
+
+'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.'
+string = 'dummy'
+
+if string in sentence:
+	print (1)
+else :
+	print (0)
---
+++ prac1.py
@@ -0,0 +1,22 @@
+import numpy as np  # type: ignore
+
+temp = []
+avg_list = []
+
+
+def moving_average(data_list, window_size):
+    print(data_list, window_size)
+
+    for i in range(len(data_list) - window_size + 1):
+        window = data_list[i:i+window_size]
+        window_average = sum(window) / window_size
+        avg_list.append(window_average)
+
+    print(avg_list)
+    return avg_list
+
+
+data = np.random.randint(10, size=5)
+window_size = 3
+
+moving_average(data, window_size)
